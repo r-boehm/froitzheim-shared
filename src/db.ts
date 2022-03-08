004e9248187d0c39a81d230cc3c4ee0d0e840992
Richard Böhm <richardmboehm@gmail.com>
2022-03-08 11:55:00 +0100

    Added db types

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,76 @@
-import { EmploymentType, Gender } from './types'
+import { ChangeReason, InsurantType } from '.'
+import { Gender } from './types'
 
-export interface Person {
-    person_id: string,
-    name: string,
-    firstname: string,
-    gender: Gender,
-    birthday: Date,
-    employment_type: EmploymentType
-    postcode: string,
-    city: string,
-    street: string,
-    phone: string,
-    email: string,
-    parent_entry: string,
-    iban: string,
-    bic: string,
-    pension_insurance_number: string,
-    health_insurance_number: string,
+export interface IPerson {
+    person_id: number;
+    name: string;
+    firstName: string;
+    birthday: Date;
+    gender: Gender;
+    postcode: string;
+    city: string;
+    street: string;
+    phone: string;
+    email: string;
+    parent_entry: IPerson;
+    iban: string;
+    bic: string;
+    pension_insurance_number: string;
+    employments: IEmployment[];
+    insurances: IInsurance[];
+    child_entries: IPerson[];
+}
+
+export interface IEmployment {
+    person: number;
+    employer: number;
+    start_date: Date;
+    end_date: Date;
+    salary: number;
+}
+
+export interface IInsurance {
+    insurance_id: number;
+    name: string;
+    type: InsurantType;
+    insurance_number: string;
+    start_date: Date;
+    end_date: Date;
+    person: IPerson;
+}
+
+export interface IReferer {
+    person: number;
+    vat: string;
+    tax_number: string;
+    Applications: IApplication[];
+}
+
+export interface IApplication {
+    application_id: number;
+    person: number;
+    insurance: number;
+    start_date: Date;
+    creation_date: Date;
+    reason: ChangeReason;
+    serialized: string;
+    referers: IReferer[];
+}
+
+
+export interface IEmployer {
+    employer_id: number;
+    name: string;
+    city: string;
+    postcode: string;
+    street: string;
+    phone: string;
+    email: string;
+    employments: IEmployment[];
+}
+
+export interface ITerms {
+    person: number;
+    date: Date;
+    data: string;
 }
