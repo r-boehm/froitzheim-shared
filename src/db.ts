4a17b11a35bc8d3b11d4eba2daa0fb81e7385bbf
Richard Böhm <richardmboehm@gmail.com>
2022-06-19 15:28:04 +0200

    insurance

--- src/db.ts
+++ src/db.ts
@@ -43,9 +43,9 @@ export interface IEmployment extends BaseEntity {
 
 export interface IInsurance extends BaseEntity {
     insurance_id: number | null;
-    name: string | null;
+    healthInsurance?: HealthInsurance
     type: InsurantType | null;
-    insurance_number: string | null;
+    insurance_number?: string | null;
     start_date: Date | null;
     end_date?: Date | null;
     person_id: number | null;
@@ -67,9 +67,8 @@ export interface IApplication extends BaseEntity {
     reason: ChangeReason | null;
     serialized?: string;
     referers?: IReferer[] | null;
+    previousInsurance?: IInsurance | null;
     insurance?: IInsurance | null;
-    insurantType?: InsurantType | null;
-    healthInsurance?: HealthInsurance
     payments: Payment[];
 }
 
