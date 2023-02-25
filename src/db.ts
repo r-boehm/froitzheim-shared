580790f2a88cc4b17b0df10d0b65c189c73995e3
Richard Böhm <richardmboehm@gmail.com>
2023-02-25 14:37:52 +0100

    provision

--- src/db.ts
+++ src/db.ts
@@ -71,6 +71,7 @@ export interface IReferer extends IPerson {
     children: IReferer[];
     allowedInsurances: number[];
     provision: number;
+    _total_provision?: number;
     /**
      * Mitarbeiterkennung
      */
